542033e686aa48ad9a3f1bd945c5b32f55c7b562
10xkiet <thao655thao655@gmail.com>
2025-04-15 17:37:34 +0700

    Update utils.py to fit the input size

--- utils.py
+++ utils.py
@@ -2,10 +2,7 @@ import numpy as np
 def read_bytes(filepath, max_len=200000):
     with open(filepath, 'rb') as f:
         bytez = list(f.read())
-        
-    if len(bytez) < max_len:
-        bytez += [0] * (max_len - len(bytez))
-    else:
-        bytez = bytez[:max_len]
+    pad = max_len - len(bytez) % max_len    
+    bytez += [0]*pad
 
-    return np.array(bytez, dtype=np.uint8)
+    return np.array(bytez, dtype=np.uint8).reshape((-1,max_len))
